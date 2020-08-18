43cf993dc07e859fc0dd42e1cb39548a1464e9f5
Zak Bibi <zak.bibi49@gmail.com>
2020-08-18 17:36:10 +0100

    Add first part of the schema and resolvers

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World!')
-});
+app.use(
+    '/graphql', 
+    graphqlHTTP({
+        schema: buildSchema(`
+            type RootQuery {
+                charProfiles: [String!]!
+            }
+
+            type RootMutation {
+                createCharProfile(name: String): String
+            }
+
+            schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+        rootValue: {
+            charProfiles: () => {
+                return ['Dex', 'Beni', 'Ohru']
+            },
+            createCharProfile: (args) => {
+                const charProfileName = args.name;
+                return charProfileName
+            }
+        },
+        graphiql: true
+    })
+);
 
 app.listen(3000);
